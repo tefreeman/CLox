26bae029a1b494b58cd586d153746e56095cb872
Trevor Freeman <tefreeman@crimson.ua.edu>
2023-11-30 11:44:15 -0600

    Manager to help unwind environments because c++ does not support finally.

---
+++ CLox/EnvironmentManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Environment.h"
+
+class EnvironmentManager {
+public:
+  inline EnvironmentManager(Environment*& currentEnvironment, Environment* newEnvironment)
+    : currentEnvironment_(currentEnvironment), previousEnvironment_(currentEnvironment) {
+    currentEnvironment_ = newEnvironment;
+  }
+
+  inline ~EnvironmentManager() {
+    // Reset the environment to its previous state
+    currentEnvironment_ = previousEnvironment_;
+  }
+
+private:
+  Environment*& currentEnvironment_; // Reference to the current environment
+  Environment* previousEnvironment_; // Store the previous environment
+};
